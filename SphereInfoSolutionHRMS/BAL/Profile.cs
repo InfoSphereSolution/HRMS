0806cb04ea23ff1fcceeec9509d7ace8944813f6
InfoSphereSolution <InfoSphereSolution@gmail.com>
2016-12-09 22:20:49 +0530

    added profile class

---
+++ SphereInfoSolutionHRMS/BAL/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace BAL
+{
+    public class Profile
+    {
+        public DataTable EmployeeProfile(LoginModel login)
+        {
+            List<SqlParameter> sqlparam = new List<SqlParameter>();
+            sqlparam.Add(new SqlParameter("@UserId", login.UserId));
+            DataTable dt = DAL.SQLHelp.ExecuteReader("Usp_GetProfile", sqlparam);
+            return dt;
+        }
+    }
+}
